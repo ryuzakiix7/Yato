c7fb9e952575b455feeb15b31d3c5115c2b3cc84
Anonymous <adi855053@gmail.com>
2023-07-03 19:25:39 +0530

    Add files via upload

--- DazaiRobot/modules/memify.py
+++ DazaiRobot/modules/memify.py
@@ -3,16 +3,19 @@ import textwrap
 
 from PIL import Image, ImageDraw, ImageFont
 
-from DazaiRobot import telethn as bot
-from DazaiRobot.events import register
+from MukeshRobot import telethn as bot
+from MukeshRobot.events import register
 
 
 @register(pattern="^/mmf ?(.*)")
 async def handler(event):
+
     if event.fwd_from:
+
         return
 
     if not event.reply_to_msg_id:
+
         await event.reply("Provide Some Text To Draw!")
 
         return
@@ -20,6 +23,7 @@ async def handler(event):
     reply_message = await event.get_reply_message()
 
     if not reply_message.media:
+
         await event.reply("```Reply to a image/sticker.```")
 
         return
@@ -28,9 +32,11 @@ async def handler(event):
 
     msg = await event.reply("```Memifying this image! ✊🏻 ```")
 
+
     text = str(event.pattern_match.group(1)).strip()
 
     if len(text) < 1:
+
         return await msg.reply("You might want to try `/mmf text`")
 
     meme = await drawText(file, text)
@@ -43,6 +49,7 @@ async def handler(event):
 
 
 async def drawText(image_path, text):
+
     img = Image.open(image_path)
 
     os.remove(image_path)
@@ -50,17 +57,21 @@ async def drawText(image_path, text):
     i_width, i_height = img.size
 
     if os.name == "nt":
+
         fnt = "ariel.ttf"
 
     else:
-        fnt = "./DazaiRobot/resources/default.ttf"
+
+        fnt = "./MukeshRobot/resources/default.ttf"
 
     m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
 
     if ";" in text:
+
         upper_text, lower_text = text.split(";")
 
     else:
+
         upper_text = text
 
         lower_text = ""
@@ -70,7 +81,9 @@ async def drawText(image_path, text):
     current_h, pad = 10, 5
 
     if upper_text:
+
         for u_text in textwrap.wrap(upper_text, width=15):
+
             u_width, u_height = draw.textsize(u_text, font=m_font)
 
             draw.text(
@@ -111,7 +124,9 @@ async def drawText(image_path, text):
             current_h += u_height + pad
 
     if lower_text:
+
         for l_text in textwrap.wrap(lower_text, width=15):
+
             u_width, u_height = draw.textsize(l_text, font=m_font)
 
             draw.text(
@@ -175,4 +190,6 @@ async def drawText(image_path, text):
     return webp_file
 
 
-__mod_name__ = "mmf"
+__mod_name__ = "Mᴍғ"
+__help__ = """ 
+⫸ /mmf <ᴛᴇxᴛ> ◉ ᴛᴏ ᴍᴇᴍɪғʏ """
