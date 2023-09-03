9013e283e1f2868d758eee6798f85a8b8250896c
Awesomeash4 <137569158+Awesomeash4@users.noreply.github.com>
2023-09-03 23:46:00 +0530

    Update alive.py

--- DazaiRobot/modules/alive.py
+++ DazaiRobot/modules/alive.py
@@ -10,7 +10,7 @@ from DazaiRobot import BOT_NAME, BOT_USERNAME, OWNER_ID, START_IMG, SUPPORT_CHAT
 @pbot.on_message(filters.command("alive"))
 async def awake(_, message: Message):
     TEXT = f"**ʜᴇʏ {message.from_user.mention},\n\nɪ ᴀᴍ {BOT_NAME}**\n━━━━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴍᴀsᴛᴇʀ :** [ᴀɴᴏɴʏᴍᴏᴜs](tg://user?id={OWNER_ID})\n\n"
+    TEXT += f"» **ᴍʏ ᴍᴀsᴛᴇʀ :** [ʟᴇᴠɪ](tg://user?id={OWNER_ID})\n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━━━━━\n\n"
