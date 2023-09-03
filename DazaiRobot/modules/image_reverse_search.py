0c7ca74df99c4121cb8e16b911ad14cabc9387d4
Awesomeash4 <137569158+Awesomeash4@users.noreply.github.com>
2023-09-03 23:44:11 +0530

    Update image_reverse_search.py

--- DazaiRobot/modules/image_reverse_search.py
+++ DazaiRobot/modules/image_reverse_search.py
@@ -1,50 +1,104 @@
-import requests
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
-from telegram.ext import CallbackContext, CommandHandler
+# Author: https://github.com/SastaDev
+# Created on: Sun, Sep 3, 2023.
+# © Kangers Network ~ https://t.me/KangersNetwork
 
-from DazaiRobot import TOKEN, dispatcher
+from typing import BinaryIO, Dict, List
+import time
+import os
+from uuid import uuid4
 
-url = "https://google-reverse-image-api.vercel.app/reverse"
+import httpx
 
+from pyrogram.enums import MessageMediaType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pyrogram import Client, filters
 
-def reverse(update: Update, context: CallbackContext):
-    if not update.effective_message.reply_to_message:
-        update.effective_message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ.")
+# Replace `YourRobot` with your robot and import pyrogram client from it.
+from DazaiRobot import pbot
 
-    elif not update.effective_message.reply_to_message.photo:
-        update.effective_message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ.")
+API_URL: str = "https://sasta-api.vercel.app/google_reverse"
 
-    elif update.effective_message.reply_to_message.photo:
-        msg = update.effective_message.reply_text("➠ sᴇᴀʀᴄʜɪɴɢ ʏᴏᴜʀ ǫᴜᴇʀʏ...")
+COMMANDS: List[str] = [
+    "reverse",
+    "grs",
+    "pp"
+    ]
 
-        photo_id = update.effective_message.reply_to_message.photo[-1].file_id
-        get_path = requests.post(
-            f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={photo_id}"
-        ).json()
-        file_path = get_path["result"]["file_path"]
-        data = {
-            "imageUrl": f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{TOKEN}/{file_path}"
-        }
+async_client: httpx.AsyncClient = httpx.AsyncClient(timeout=120)
 
-        response = requests.post(url, json=data)
-        result = response.json()
-        if response.ok:
-            msg.edit_text(
-                f"[{result['data']['resultText']}]({result['data']['similarUrl']})",
-                parse_mode=ParseMode.MARKDOWN,
-                reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("ʟɪɴᴋ", url="https://t.me/The_Apexx")]]
-                ),
-            )
-        else:
-            update.effective_message.reply_text("Some exception occured")
+class STRINGS:
+    REPLY_TO_MEDIA: str = "ℹ️ Please reply to a message that contains one of the supported media types, such as a photo, sticker, or image file."
+    UNSUPPORTED_MEDIA_TYPE: str = "⚠️ <b>Unsupported media type!</b>\nℹ️ Please reply with a supported media type: image, sticker, or image file."
+    
+    DOWNLOADING_MEDIA: str = "⏳ Downloading media..."
+    UPLOADING_TO_API_SERVER: str = "📡 Uploading media to <b>API Server</b>... 📶"
+    PARSING_RESULT: str = "💻 Parsing result..."
+    
+    EXCEPTION_OCCURRED: str = "❌ <b>Exception occurred!</b>\n\n<b>Exception:</b> {}"
+    
+    RESULT: str = """
+🔤 <b>Query:</b> <code>{query}</code>
+🔗 <b>Page Link:</b> <a href="{page_url}">Link</a>
 
-reverse_cmd = CommandHandler(
-    ["grs", "reverse", "pp", "p", "P"], reverse, run_async=True
-)
-dispatcher.add_handler(reverse_cmd)
+⌛️ <b>Time Taken:</b> <code>{time_taken}</code> seconds.
+🧑‍💻 <b>Credits:</b> @KangersNetwork
+    """
+    OPEN_PAGE: str = "↗️ Open Page"
 
-__mod_name__ = "ʀᴇᴠᴇʀsᴇ"
-__help__ = """
-~ /reverse |pp |grs |p : Does a reverse image search of the media which it was replied to.
-"""
+@pbot.on_message(filters.command(COMMANDS))
+async def on_reverse(client: Client, message: Message) -> None:
+    if not message.reply_to_message:
+        await message.reply(STRINGS.REPLY_TO_MEDIA)
+        return
+    elif message.reply_to_message.media not in (MessageMediaType.PHOTO, MessageMediaType.STICKER, MessageMediaType.DOCUMENT):
+        await message.reply(STRINGS.UNSUPPORTED_MEDIA_TYPE)
+        return
+    
+    start_time: float = time.time()
+    status_msg: Message = await message.reply(STRINGS.DOWNLOADING_MEDIA)
+    file_path: str = f"temp_download/{uuid4()}"
+    try:
+        await message.reply_to_message.download(file_path)
+    except Exception as exc:
+        text: str = STRINGS.EXCEPTION_OCCURRED.format(exc)
+        await message.reply(text)
+        try:
+            os.remove(file_path)
+        except FileNotFoundError:
+            pass
+        return
+    
+    await status_msg.edit(STRINGS.UPLOADING_TO_API_SERVER)
+    files: Dict[str, BinaryIO] = {"file": open(file_path, "rb")}
+    response: httpx.Response = await async_client.post(API_URL, files=files)
+    os.remove(file_path)
+    
+    if response.status_code == 404:
+        text: str = STRINGS.EXCEPTION_OCCURRED.format(response.json()["error"])
+        await message.reply(text)
+        await status_msg.delete()
+        return
+    elif response.status_code != 200:
+        text: str = STRINGS.EXCEPTION_OCCURRED.format(response.text)
+        await message.reply(text)
+        await status_msg.delete()
+        return
+    
+    await status_msg.edit(STRINGS.PARSING_RESULT)
+    response_json: Dict[str, str] = response.json()
+    query: str = response_json["query"]
+    page_url: str = response_json["url"]
+    
+    end_time: float = time.time() - start_time
+    time_taken: str = "{:.2f}".format(end_time)
+    
+    text: str = STRINGS.RESULT.format(
+        query=query,
+        page_url=page_url,
+        time_taken=time_taken
+        )
+    buttons: List[List[InlineKeyboardButton]] = [
+        [InlineKeyboardButton(STRINGS.OPEN_PAGE, url=page_url)]
+        ]
+    await message.reply(text, disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))
+    await status_msg.delete()
