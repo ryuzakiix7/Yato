eb6b6038e5f3e47fd5d95119749c8727bb8204af
Awesomeash4 <137569158+Awesomeash4@users.noreply.github.com>
2023-09-03 23:27:58 +0530

    Update errors.py

--- DazaiRobot/utils/errors.py
+++ DazaiRobot/utils/errors.py
@@ -1,5 +1,30 @@
+"""
+BSD 2-Clause License
+Copyright (C) 2017-2019, Paul Larsen
+Copyright (C) 2023-2024, Eren-YeagerO, [ https://github.com/Eren-YeagerO]
+Copyright (c) 2023-2024, Programmer Network, [ https://github.com/Eren-YeagerO/Hori ]
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 import sys
-import traceback
+import traceback, asyncio
 from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
@@ -24,6 +49,15 @@ def split_limits(text):
     result.append(small_msg)
 
     return result
+
+
+def asyncify(func):
+    async def inner(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        func_out = await loop.run_in_executor(None, func, *args, **kwargs)
+        return func_out
+
+    return inner
 
 
 def capture_err(func):
