04ba5d3bf5cb3698e5d15836de65163fcfd6737e
Shaurya <139495856+Shauryanoobhai@users.noreply.github.com>
2023-09-12 10:14:39 +0530

    Update source.py

--- DazaiRobot/modules/source.py
+++ DazaiRobot/modules/source.py
@@ -17,7 +17,7 @@ async def repo(_, message: Message):
 
 ɪ ᴀᴍ [{BOT_NAME}](https://t.me/{BOT_USERNAME})**
 
-**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** ᴀɴᴏɴʏᴍᴏᴜs
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ :** Levi
 **» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
 **» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
 **» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
@@ -29,7 +29,7 @@ async def repo(_, message: Message):
                     InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=OWNER_ID),
                     InlineKeyboardButton(
                         "sᴏᴜʀᴄᴇ",
-                        url="https://github.com/Anonymous-068/DazaiRobot",
+                        url="https://github.com/Anonymous-068/Kakashiii",
                     ),
                 ]
             ]
