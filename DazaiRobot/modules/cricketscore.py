ab2a4bdd13d14b96749c78b04ba5a1c941f1d37a
Awesomeash4 <137569158+Awesomeash4@users.noreply.github.com>
2023-09-16 20:36:46 +0530

    Update cricketscore.py

--- DazaiRobot/modules/cricketscore.py
+++ DazaiRobot/modules/cricketscore.py
@@ -1,39 +1,12 @@
-""" 
- STATUS: Code is working. ✅ 
- """ 
-  
- """ 
- GNU General Public License v3.0 
-  
- Copyright (C) 2022, SOME-1HING [https://github.com/SOME-1HING] 
-  
- Credits:- 
-     I don't know who originally wrote this code. If you originally wrote this code, please reach out to me.  
-  
- This program is free software: you can redistribute it and/or modify 
- it under the terms of the GNU General Public License as published by 
- the Free Software Foundation, either version 3 of the License, or 
- (at your option) any later version. 
-  
- This program is distributed in the hope that it will be useful, 
- but WITHOUT ANY WARRANTY; without even the implied warranty of 
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- GNU General Public License for more details. 
-  
- You should have received a copy of the GNU General Public License 
- along with this program.  If not, see <https://www.gnu.org/licenses/>. 
- """ 
-  
- import urllib.request 
-  
- from bs4 import BeautifulSoup 
- from telethon import events 
- from DazaiRobot import telethn as tbot 
- from telethon.tl import functions, types 
- from telethon.tl.types import * 
+import urllib.request 
+from bs4 import BeautifulSoup 
+from telethon import events 
+from DazaiRobot import telethn as tbot 
+from telethon.tl import functions, types  
+from telethon.tl.types import * 
   
   
- async def is_register_admin(chat, user): 
+async def is_register_admin(chat, user): 
      if isinstance(chat, (types.InputPeerChannel, types.InputChannel)): 
          return isinstance( 
              ( 
@@ -45,7 +18,7 @@
          return True 
   
   
- @tbot.on(events.NewMessage(pattern="/(c|cs)$")) 
+@tbot.on(events.NewMessage(pattern="/(c|cs)$")) 
  async def _(event): 
      if event.fwd_from: 
          return 
