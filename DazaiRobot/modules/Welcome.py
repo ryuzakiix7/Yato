129c5bfde7fc480bdbe170ce61d859ff95513347
Kale Rajesh Kakasaheb <93943699+Blank-sama@users.noreply.github.com>
2023-12-10 18:22:18 +0530

    Update Welcome.py

--- DazaiRobot/modules/Welcome.py
+++ DazaiRobot/modules/Welcome.py
@@ -1,19 +1,42 @@
-from datetime import datetime, timedelta
-import time
-import pytz
-import os
+mport os
+from unidecode import unidecode
+from PIL import ImageDraw, Image, ImageFont, ImageChops
+from pyrogram import *
+from pyrogram.types import *
 from logging import getLogger
-from PIL import Image, ImageChops, ImageDraw, ImageFont
-import textwrap
-from pyrogram import filters, Client
-from pyrogram.types import ChatMemberUpdated
-from DazaiRobot import pbot as app
-from DazaiRobot.utils.errors import capture_err, asyncify
-from DazaiRobot.utils.utils import temp
+from pymongo import MongoClient
+from DazaiRobot import MONGO_DB_URI 
+from DazaiRobot import pbot as app, LOG_CHANNEL_ID
+COMMAND_HANDLER = ". /".split()
 
-LOGGER = getLogger(__name__)
 
-def circle(pfp, size=(215, 215)):
+
+client = MongoClient(MONGO_DB_URI)
+dbnaam = client["Aaru"]
+
+wlcm = dbnaam['welcome']
+
+
+async def add_wlcm(chat_id : int):
+    return await wlcm.insert_one({"chat_id" : chat_id})
+    
+async def rm_wlcm(chat_id : int):   
+    chat = await wlcm.find_one({"chat_id" : chat_id})
+    if chat: 
+        return await wlcm.delete_one({"chat_id" : chat_id})
+
+
+LOGGER = getLogger(name)
+
+class temp:
+    ME = None
+    CURRENT = 2
+    CANCEL = False
+    MELCOW = {}
+    U_NAME = None
+    B_NAME = None
+
+def circle(pfp, size=(450, 450)):
     pfp = pfp.resize(size, Image.ANTIALIAS).convert("RGBA")
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
     mask = Image.new("L", bigsize, 0)
@@ -24,78 +47,125 @@ def circle(pfp, size=(215, 215)):
     pfp.putalpha(mask)
     return pfp
 
-def draw_multiple_line_text(image, text, font, text_start_height):
-    draw = ImageDraw.Draw(image)
-    image_width, image_height = image.size
-    y_text = text_start_height
-    lines = textwrap.wrap(text, width=50)
-    for line in lines:
-        line_width, line_height = font.getsize(line)
-        draw.text(((image_width - line_width) / 2, y_text), line, font=font, fill="black")
-        y_text += line_height
-
-@asyncify
-def welcomepic(pic, user, chat, count, id):
-    new_count = count + 1
-
-    background = Image.open("img/bg.png")  # <- Background Image (Should be PNG)
-    background = background.resize((1024, 500), Image.ANTIALIAS)
+def welcomepic(pic, user, chat, id, uname):
+    background = Image.open("HuTao/resources/bg.jpg")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
-    pfp = pfp.resize((265, 265))  # Resizes the Profilepicture so it fits perfectly in the circle
-    font = ImageFont.truetype("Calistoga-Regular.ttf", 37)  # <- Text Font of the Member Count. Change the text size for your preference
-    member_text = f"Welcome {user}"  # <- Text under the Profilepicture with the Membercount
-    draw_multiple_line_text(background, member_text, font, 395)
-    draw_multiple_line_text(background, chat, font, 47)
-    ImageDraw.Draw(background).text(
-        (530, 460),
-        f"You Are {new_count}th Member Here",
-        font=ImageFont.truetype("Calistoga-Regular.ttf", 28),
-        size=20,
-        align="right",
+    pfp = pfp.resize(
+        (450, 450)
+    ) 
+    draw = ImageDraw.Draw(background)
+    font = ImageFont.truetype('HuTao/resources/SwanseaBold-D0ox.ttf', size=40)
+    welcome_font = ImageFont.truetype('HuTao/resources/SwanseaBold-D0ox.ttf', size=60)
+    draw.text((30, 300), f'NAME: {unidecode(user)}', fill=(255, 255, 255), font=font)
+    draw.text((30, 370), f'ID: {id}', fill=(255, 255, 255), font=font)
+    draw.text((30, 40), f"Welcome to {unidecode(chat)}", fill=(225, 225, 225), font=welcome_font)
+    draw.text((30,430), f"USERNAME : {uname}", fill=(255,255,255),font=font)
+    pfp_position = (770, 140)  
+    background.paste(pfp, pfp_position, pfp)  
+    background.save(
+        f"downloads/welcome#{id}.png"
     )
-    background.paste(pfp, (379, 123), pfp)  # Pastes the Profilepicture on the Background Image
-    welcome_filename = f"downloads/welcome#{id}.png"
-    background.save(welcome_filename)  # Saves the finished Image in the folder with the filename
-    return welcome_filename
+    return f"downloads/welcome#{id}.png"
 
-@app.on_chat_member_updated(filters.group)
-@capture_err
-async def member_has_joined(_, member: ChatMemberUpdated):
+
+@app.on_message(filters.command("swelcome", COMMAND_HANDLER) & ~filters.private)
+async def auto_state(_, message):
+    usage = "Usage:\n/swelcome [ENABLE|DISABLE]"
+    if len(message.command) == 1:
+        return await message.reply_text(usage)
+    chat_id = message.chat.id
+    user = await app.get_chat_member(message.chat.id, message.from_user.id)
+    if user.status in (
+        enums.ChatMemberStatus.ADMINISTRATOR,
+        enums.ChatMemberStatus.OWNER,
+    ):
+      A = await wlcm.find_one({"chat_id" : chat_id})
+      state = message.text.split(None, 1)[1].strip()
+      state = state.lower()
+      if state == "enable":
+        if A:
+           return await message.reply_text("Special Welcome Already Enabled")
+        elif not A:
+           await add_wlcm(chat_id)
+           await message.reply_text(f"Enabled Special Welcome in {message.chat.title}")
+      elif state == "disable":
+        if not A:
+           return await message.reply_text("Special Welcome Already Disabled")
+        elif A:
+           await rm_wlcm(chat_id)
+           await message.reply_text(f"Disabled Special Welcome in {message.chat.title}")
+      else:
+        await message.reply_text(usage)
+    else:
+        await message.reply("Only Admins Can Use This Command")
+
+@app.on_chat_member_updated(filters.group, group=-3)
+async def greet_group(_, member: ChatMemberUpdated):
+    chat_id = member.chat.id
+    A = await wlcm.find_one({"chat_id" : chat_id})
+    if not A:
+       return
     if (
         not member.new_chat_member
         or member.new_chat_member.status in {"banned", "left", "restricted"}
         or member.old_chat_member
     ):
         return
-    
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
-    if user.is_bot:
-        return  # Ignore bots
-    
-    mention = f"<a href='tg://user?id={user.id}'>{user.first_name}</a>"
-    timezone = pytz.timezone("Asia/Kolkata")
-    joined_date = datetime.fromtimestamp(time.time(), tz=timezone).strftime("%d %B %Y %I:%M:%S %p")
-    first_name = f"{user.first_name} {user.last_name}" if user.last_name else user.first_name
-    id = user.id
-    dc = user.dc_id or "Member tanpa PP"
-    count = await app.get_chat_members_count(member.chat.id)
-    
     try:
-        pic = await app.download_media(user.photo.big_file_id, file_name=f"pp{user.id}.png")
+        pic = await app.download_media(
+            user.photo.big_file_id, file_name=f"pp{user.id}.png"
+        )
     except AttributeError:
-        pic = "img/profilepic.png"
-    
-    welcomeimg = await welcomepic(pic, user.first_name, member.chat.title, count, user.id)
-    await app.send_photo(
-        member.chat.id,
-        photo=welcomeimg,
-        caption=f"<b>Hey</b> <b>{mention}</b>, <b>Welcome to the Group {member.chat.title}!</b>\n\n<b>Name:</b> <code>{first_name}</code>\n<b>ID:</b> <code>{id}</code>\n<b>Join Date:</b> <code>{joined_date}</code>",
-    )
-    
+        pic = "HuTao/resources/profilepic.jpg"
+    if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
+        try:
+            await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
+            except Exception as e:
+            LOGGER.error(e)
     try:
-        os.remove(welcomeimg)
+        welcomeimg = welcomepic(
+            pic, user.first_name, member.chat.title, user.id, user.username
+        )
+        temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
+            member.chat.id,
+            photo=welcomeimg,
+            caption= f"""
+**WELCOME TO {member.chat.title}
+➖➖➖➖➖➖➖➖➖➖➖➖
+NAME: {user.mention}
+ID: {user.id}
+USERNAME: @{user.username}
+➖➖➖➖➖➖➖➖➖➖➖➖**
+""",
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton (f"{user.first_name}", url=f"https://t.me/{user.username}")]])
+
+            )
+    except Exception as e:
+        LOGGER.error(e)
+    try:
+        os.remove(f"downloads/welcome#{user.id}.png")
         os.remove(f"downloads/pp{user.id}.png")
-    except Exception:
+    except Exception as e:
         pass
-        
+
+
+@app.on_message(filters.new_chat_members & filters.group, group=-1)
+async def bot_wel(_, message):
+    for u in message.new_chat_members:
+        if u.id == app.me.id:
+            await app.send_message(LOG_CHANNEL_ID, f"""
+**NEW GROUP
+➖➖➖➖➖➖➖➖➖➖➖➖
+NAME: {message.chat.title}
+ID: {message.chat.id}
+USERNAME: @{message.chat.username}
+➖➖➖➖➖➖➖➖➖➖➖➖**
+""")
+
+
+mod = "WELCOME"
+help = """
+» /swelcome - Turn On The Special Welcome For Groups
+"""
